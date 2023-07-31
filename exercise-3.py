75f771d16d140fbb6a23ad4aaf99c5ee18e1490c
Heavseima <2023440heav@aupp.edu.kh>
2023-07-31 22:52:59 +0700

    gg

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,10 @@
 def remove_all_after(numbers, n):
-    ...
+    if len(numbers) == 0: 
+        return []
+    
+    for i, index in enumerate(numbers):  
+        if index == n:
+            return numbers[:i+1]
+    
+    return numbers  
+
