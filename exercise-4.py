93d3bd2e1d4e3f8cd631360de29464eefe22df49
sopheavatey <2023363heang@aupp.edu.kh>
2023-08-01 22:09:13 +0700

    try again

--- exercise-4.py
+++ exercise-4.py
@@ -1,7 +1,10 @@
 def chunking_by(numbers, chunck):
-    if chunck == 0:
-        return "0 is not a possible chuncking value!!!"
+    if chunck <= 0 or not isinstance(chunck, int):
+        return "chunk size need to be a positive integer"
+    try:
+        isinstance (chunck, int)
 
+    except 
     chunks = []
     for i in range(0,len(numbers), chunck):
         # slice the original list to get the wanted chunk
@@ -11,6 +14,6 @@ def chunking_by(numbers, chunck):
 
     return chunks
 
-
+print(chunking_by([0,1,4,2,6,8,4,2], uu))
         
     
