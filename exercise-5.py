a0ce5a0eead6b3e8142148a54c2c5e43666977f1
Channeath Ros <2023316ros@aupp.edu.kh>
2023-08-01 11:21:03 +0700

    ex5

--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,7 @@
 def reverse_ascending(numbers):
-    ...
+    lists = []
+    for i in numbers:
+        lists.append(i)
+    return sorted(lists, reverse = True)
+    
+print(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]))
