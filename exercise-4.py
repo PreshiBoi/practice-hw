0ac700bedd7a43480f9b4178de9f1e5c51dacf81
sopheavatey <2023363heang@aupp.edu.kh>
2023-08-01 21:38:40 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,3 +1,16 @@
 def chunking_by(numbers, chunck):
-    chunk = []
+    if chunck == 0:
+        return "0 is not a possible chuncking value!!!"
+
+    chunks = []
+    for i in range(0,len(numbers), chunck):
+        # slice the original list to get the wanted chunk
+        divide = numbers[i:i + chunck]
+        # add the value to the list
+        chunks.append(divide)
+
+    return chunks
+
+
+        
     
